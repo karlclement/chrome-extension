6cf9e6d49c587ca252f75acfa509b35786f84cc7
François Grante <grante.francois@gmail.com>
2015-07-01 13:58:48 +0200

    Confidence score implementation in Linkedin popup

--- content_script/linkedin-popup.js
+++ content_script/linkedin-popup.js
@@ -77,7 +77,7 @@ function openPopup(callback) {
   var windowHeight = $(window).height();
   var windowWidth = $(window).width();
 
-  $("body").append('<div id="eh_popup"><i class="fa fa-ellipsis-v eh_popup_drag"></i><div class="eh_popup_close">&times;</div><div class="eh_popup_name">' + window.first_name + ' ' + window.last_name + '</div><div class="eh_popup_job_title">' + window.job_title + '</div><div id="eh_popup_error"></div><form id="eh_popup_ask_domain"><div id="eh_popup_ask_domain_message"></div><input placeholder="company.com" id="eh_popup_ask_domain_field" type="text" name="domain"><button class="clear_cta" type="submit">Find</button></form><div id="eh_popup_content_container"><div id="eh_popup_content"></div></div><div id="eh_popup_results_link_container"></div><div id="eh_popup_results_show"></div></div>');
+  $("body").append('<div id="eh_popup"><i class="fa fa-ellipsis-v eh_popup_drag"></i><div class="eh_popup_close">&times;</div><div class="eh_popup_name">' + window.first_name + ' ' + window.last_name + '</div><div class="eh_popup_job_title">' + window.job_title + '</div><div id="eh_popup_error"></div><form id="eh_popup_ask_domain"><div id="eh_popup_ask_domain_message"></div><input placeholder="company.com" id="eh_popup_ask_domain_field" type="text" name="domain"><button class="clear_cta" type="submit">Find</button></form><div id="eh_popup_content_container"><div id="eh_popup_content"></div></div><div class="eh_popup_confidence_score"></div><div id="eh_popup_results_link_container"></div><div id="eh_popup_results_show"></div></div>');
 
   $("#eh_popup")
     .css({
@@ -123,6 +123,8 @@ function launchSearch() {
             }
             else {
               mainMessagePopup(json.email);
+              showConfidence(json.score);
+
               $("#eh_popup_results_link_container").show();
             }
           });
@@ -182,6 +184,13 @@ function mainMessagePopup(message, loader) {
   else { loader_html = ''; }
 
   $("#eh_popup_content").html(loader_html + message);
+}
+
+
+// Show confidence score
+//
+function showConfidence(score) {
+  $(".eh_popup_confidence_score").html('<div class="eh_popup_confidence">' + score + '% confidence</div><div class="eh_popup_confidence_bar"><div class="eh_popup_confidence_level" style="width: ' + score + '%;"></div></div>');
 }
 
 
