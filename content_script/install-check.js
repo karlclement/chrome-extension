60285f7ccf01ff0bac6627aa13a4bdddf11281ed
François Grante <grante.francois@gmail.com>
2015-07-03 10:48:10 +0200

    Installation check on Email Hunter website

---
+++ content_script/install-check.js
@@ -0,0 +1,4 @@
+// Update install buttons on Email Hunter website
+//
+$('.install-chrome').addClass('disabled');
+$('.install-chrome').html('<i class="fa fa-check" style="margin-right: 10px;"></i>Installed');
